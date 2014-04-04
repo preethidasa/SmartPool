db95a5b970c78e9099b59965a4df2e6a54fdf76f
sumitjas88 <sumitjas88@gmail.com>
2014-04-04 12:40:17 +0530

    sumitnew

---
+++ src/main/java/com/tw/smartpool/model/CarPool.java
@@ -0,0 +1,9 @@
+package com.tw.smartpool.model;
+
+/**
+ * Created by preethid on 01/04/14.
+ */
+public class CarPool {
+
+    
+}
---
+++ src/main/java/com/tw/smartpool/model/User.java
@@ -0,0 +1,7 @@
+package com.tw.smartpool.model;
+
+/**
+ * Created by sumitjai on 04/04/14.
+ */
+public class User {
+}
