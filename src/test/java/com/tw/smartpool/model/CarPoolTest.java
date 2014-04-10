49972fac2dd6785d576ae6b0a1085ee6dc9a93c4
sumitjas88 <sumitjas88@gmail.com>
2014-04-10 16:55:16 +0530

    Sumit/Renjith Refactored

--- src/test/java/com/tw/smartpool/model/CarPoolTest.java
+++ src/test/java/com/tw/smartpool/model/CarPoolTest.java
@@ -11,16 +11,23 @@ public class CarPoolTest {
    @Test
     public void shouldReturnListOfAllCarsGoingThroughPoint() {
        Car[] c = new Car[3];
-       Route r = new Route(1,2, new String[]{"Bellandur","ThoughtWorks"});
+       String destination = "ThoughtWorks";
+       Route r = createRoute("Bellandur", destination);
        c[0] = new Car("KA 01 4567",6,r);
-       r = new Route(1,2, new String[]{"Btm","ThoughtWorks"});
+       r = createRoute("Btm", destination);
        c[1] = new Car("KA 01 4568",6,r);
-       r = new Route(1,2, new String[]{"Marathalli","Itpl"});
+       r = createRoute("Marathalli", "Itpl");
        c[2] = new Car("KA 01 4569",6,r);
+
        CarPool cp = new CarPool(3,c);
-       Car[] result = cp.carsPassingThroughPoint("ThoughtWorks");
+       Car[] result = cp.carsPassingThroughPoint(destination);
+
        assertTrue( c[0].equals(result[0]) );
        assertTrue( c[1].equals(result[1]) );
        assertEquals( null, result[2] );
    }
+
+    private Route createRoute(String point, String destination) {
+        return new Route(1,2, new String[]{point, destination});
+    }
 }
